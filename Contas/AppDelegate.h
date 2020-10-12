31cb8f0c78456f765428487fa76a13cdadffa739
Gabriel Alencar <gabriel@inking.com.br>
2020-10-12 01:04:28 -0300

    Initial Commit

---
+++ Contas/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Contas
+//
+//  Created by Gabriel Alencar on 12/10/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Contas/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Contas
+//
+//  Created by Gabriel Alencar on 12/10/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
